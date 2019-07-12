729d46bd508a37d012474083a270c59adbc7b7ae
Reese Kunz <reesekunz21@gmail.com>
2019-07-12 11:50:02 -0700

    fixed headline uppercase error, cleaned up formatting

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -18,7 +18,6 @@
 //
 // Create a card for each of the articles and add the card to the DOM.
 
-
 // select the main dom node to attach our dynamic content
 
 const cards = document.querySelector(".cards-container");
@@ -34,59 +33,53 @@ axios
 
     //console.log("Handles success", articles);
 
-    // Use Object.keys to create array 
+    // Use Object.keys to create array
     const articleTopics = Object.keys(articles);
     console.log("topics", articleTopics);
 
-
-    //You will be creating a component for each 'article' in the list.   
+    //You will be creating a component for each 'article' in the list.
 
     // Use forEach to iterate through array created through Object.keys above
     articleTopics.forEach(topic => {
-    // Use another forEach to add component for each 'article' in the list.  
-    articles[`${topic}`].forEach(article => {
-    // Add to DOM 
-    cards.append(createCards(article));
-
-});
-});
-})
-.catch(error => {
+      // Use another forEach to add component for each 'article' in the list.
+      articles[`${topic}`].forEach(article => {
+        // Add to DOM
+        cards.append(createCards(article));
+      });
+    });
+  })
+  .catch(error => {
     // Handles failure:
     console.log("The API is currently down, try again later ", error);
   });
 
+function createCards(topic) {
+  // create the elements based off HTML
+  const card = document.createElement("div");
+  const headline = document.createElement("div");
+  const author = document.createElement("div");
+  const imageContainer = document.createElement("div");
+  const image = document.createElement("img");
+  const authorName = document.createElement("span");
 
-  function createCards(topic) {
+  //set the styles based off classes in HTML
 
-// create the elements based off HTML
-const card = document.createElement("div");
-const headLine = document.createElement("div");
-const author = document.createElement("div");
-const imageContainer = document.createElement("div");
-const image = document.createElement("img");
-const authorName = document.createElement("span");
+  card.classList.add("card");
+  headline.classList.add("headline");
+  author.classList.add("author");
+  imageContainer.classList.add("img-container");
 
-//set the styles based off classes in HTML
+  //set the content based (HTML on left, API info on right)
+  headline.textContent = topic.headline;
+  image.src = topic.authorPhoto;
+  authorName.textContent = topic.authorName;
 
-card.classList.add("card");
-headLine.classList.add("headline");
-author.classList.add("author");
-imageContainer.classList.add("img-container");
+  // put together based off HTML parent/child
+  card.append(headline);
+  card.append(author);
+  author.append(imageContainer);
+  author.append(authorName);
+  imageContainer.append(image);
 
-//set the content based (HTML on left, API info on right)
-headline.textContent = topic.headline;
-img.src = topic.authorPhoto;
-authorName.textContent = topic.authorName;
-
-// put together based off HTML parent/child
-card.append(headLine);
-card.append(author);
-author.append(imageContainer);
-author.append(authorName);
-imgContainer.append(image);
-
-return card;
-
-  }
-
+  return card;
+}
