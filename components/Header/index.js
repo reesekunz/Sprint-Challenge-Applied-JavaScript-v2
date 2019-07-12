d47ddae3c30115edcc730ae98b131a8213c4c580
Reese Kunz <reesekunz21@gmail.com>
2019-07-12 08:59:46 -0700

    added function for header, still WIP

--- components/Header/index.js
+++ components/Header/index.js
@@ -7,6 +7,37 @@
 //    <h1>Lambda Times</h1>
 //    <span class="temp">98°</span>
 //  </div >
-// And add it to the DOM in the .headerContainer component
 
-function Header() {}
+const headerContainer = document.querySelector(".header-container");
+
+// creates and returns DOM node
+
+function Header() {
+  // create the elements based off HTML
+  const headerClass = document.createElement("div");
+  const date = document.createElement("span");
+  const lambdaHeader = document.createElement("h1");
+  const temp = document.createElement("span");
+
+  // set the styles based off classes
+  headerClass.classList.add("header");
+  date.classList.add("date");
+  temp.classList.add("temp");
+
+  // set the content based off HTML template
+  date.textContent = "MARCH 28, 2019";
+  lambdaHeader.textContent = "Lambda Times";
+  temp.textContent = "98°";
+
+  // put together based off HTML parent/child
+
+  headerClass.appendChild(date);
+  headerClass.appendChild(lambdaHeader);
+  headerClass.appendChild(temp);
+
+  return headerClass;
+};
+
+// Add to DOM in the .header-container component
+const headerContainer = document.querySelector(".header-container");
+headerContainer.appendChild(Header);
